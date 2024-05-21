8cd0711c835ef5f38a728d948574814217cf34f4
WrongNicholas <mnwojnar2000@gmail.com>
2024-05-21 03:22:54 -0400

    Add String formatting to displayed time and implement clear button

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -4,26 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Stopwatch implements ActionListener, Runnable {
+
     public static void main(String[] args) {
         new Stopwatch();
     }
     Thread thread;
+
     boolean running = false;
     float time = 0;
-    JLabel label;
-    JButton button;
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        running = !running;
-        if(running) {
-            button.setText("Stop");
 
-        }
-        else {
-            button.setText("Start");
-            time = 0;
-        }
-    }
+    JLabel label;
+    JButton startButton, resetButton;
 
     public Stopwatch() {
         initializeInterface();
@@ -32,36 +23,38 @@ public class Stopwatch implements ActionListener, Runnable {
         thread.start();
     }
 
-    private void initializeInterface() {
-        JFrame frame = new JFrame("Stopwatch");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(400,200);
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1,2));
+        JButton button = (JButton) e.getSource();
+        if (button == startButton) {
+            running = !running;
 
-        Font font = new Font("Dialogue", Font.PLAIN, 40);
+            if (running) {
+                startButton.setText("Stop");
+                return;
+            }
 
-        button = new JButton("Start");
-        panel.add(button);
+            startButton.setText("Stop");
+            return;
+        }
 
-        button.setFont(font);
+        reset();
+    }
 
+    private String getFormattedTime() {
+        int totalSeconds = (int) (time / 1000);
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+        int milliseconds = (int) (time % 1000);
 
-        JPanel labelPanel = new JPanel();
-        labelPanel.setLayout(new FlowLayout(FlowLayout.RIGHT,0,75));
+        return String.format("%02d", minutes) + ":" + String.format("%02d", seconds) + ":" + String.format("%03d", milliseconds);
+    }
 
-
-        label = new JLabel("0");
-        labelPanel.add(label);
-        panel.add(labelPanel);
-        label.setFont(font);
-
-        frame.add(panel);
-        frame.setVisible(true);
-
-        button.addActionListener(this);
+    private void reset() {
+        time = 0;
+        label.setText("00:00:000");
+        running = false;
     }
 
     @Override
@@ -69,14 +62,61 @@ public class Stopwatch implements ActionListener, Runnable {
         while (true){
             try {
                 Thread.sleep(10);
-                if(running){
-                    time += 10;
-                    label.setText(String.valueOf(time));
-                }
+
             } catch (InterruptedException e) {
+
                 throw new RuntimeException(e);
             }
 
+            if (running){
+                time += 10.f;
+                label.setText(getFormattedTime());
+            }
         }
+    }
+
+    private void initializeInterface() {
+        // Font
+        Font font = new Font("Dialogue", Font.PLAIN, 40);
+
+        // Frame
+        JFrame frame = new JFrame("Stopwatch");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(400,200);
+
+        // Main Panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1,2));
+
+        // Button Panel
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(2,1));
+
+        // Buttons
+        startButton = new JButton("Start");
+        startButton.setFont(font);
+        startButton.addActionListener(this);
+        buttonPanel.add(startButton);
+
+        resetButton = new JButton("Reset");
+        resetButton.setFont(font);
+        resetButton.addActionListener(this);
+        buttonPanel.add(resetButton);
+
+        panel.add(buttonPanel);
+
+        // Label Panel
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new BorderLayout());
+
+        // Label
+        label = new JLabel("00:00:000");
+        label.setFont(font);
+        labelPanel.add(label, BorderLayout.EAST);
+
+        panel.add(labelPanel);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
